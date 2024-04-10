287e7e0b8d67298921c6e582825eb114e1837253
Allesandria <56239731+Abargallo19@users.noreply.github.com>
2024-04-10 18:32:47 -0400

    Submission of initial python lab

---
+++ P1LAB1_BargalloAllesandria.py
@@ -0,0 +1,10 @@
+# Bargallo, Allesandria
+# April 10, 2024
+# P1LAB1
+# Small program to allow user to input name to display welcome message
+
+firstName = input('Enter your first name:')
+lastName = input('Enter your last name:')
+welcome = 'Hello, ' + firstName + ' ' + lastName + '! Welcome to CTI-110'
+
+print(welcome)
