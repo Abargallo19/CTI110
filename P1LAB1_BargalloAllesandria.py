690ee13189ced756ce76b1af1ccc53204bcd7b5e
Allesandria <56239731+Abargallo19@users.noreply.github.com>
2024-09-23 22:06:10 -0400

    Add files via upload

---
+++ P1LAB1_BargalloAllesandria.py
@@ -0,0 +1,14 @@
+
+#Allesandria Bargallo
+#23 September 2024
+#P1LAB1
+#Simple welcome program
+
+
+
+
+first_name = input("Please enter your first name: ")
+last_name = input("Please enter your last name: ")
+
+
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
